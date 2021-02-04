aed3f588b017e14f94275f779cb587ad6ddbb94d
birsen-dlms <birsen.dlms.80@gmail.com>
2021-02-04 17:07:38 +0300

    20 Ocak Ders Sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            //geciciDizi
+            T[] tempArray = items;//böyle yapınca elemanlar kaybolmamış olur.elemanları geçici bir yerde tuttu. 
+            items = new T[items.Length + 1];//dizinin eleman sayısını 1 arttırdı.
+            //for ile geçicideki elemanları geri alıyor.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+    }
+}
